240e565acb79d96d6fc4ae1f2fa8f861e58c9731
aang083 <golen46@gmail.com>
2022-12-22 23:36:56 +0100

    tworzenie poprawnej struktury plikow c.d.

--- ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Klienta/Klient.cshtml.cs
+++ ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Klienta/Klient.cshtml.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Narciarze_v_2.Pages
+namespace Narciarze_v_2.Pages.Strefa_Klienta
 {
-    public class LogowanieModel : PageModel
+    public class KlientModel : PageModel
     {
         public void OnGet()
         {
         }
-
-
     }
 }
