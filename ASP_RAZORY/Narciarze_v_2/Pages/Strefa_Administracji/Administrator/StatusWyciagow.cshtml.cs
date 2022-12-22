f4b0591912f0ec7834e1466328896deaee665a6f
aang083 <golen46@gmail.com>
2022-12-22 23:02:56 +0100

    Stworzenie struktury opartej o technologie RAZOR

---
+++ ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Administracji/Administrator/StatusWyciagow.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Narciarze_v_2.Pages.Strefa_Administracji.Administrator
+{
+    public class StatusWyciagowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Administracji/Zarzad/ZmianaCennika.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Narciarze_v_2.Pages.Strefa_Administracji.Zarzad
+{
+    public class ZmianaCennikaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
