ed3ec0a831a59b7cd43df8f63635cd3454c56890
Piotr Pluta <piotplu356@studia.polsl.pl>
2022-12-23 11:31:11 +0100

    Update Frontend

---
+++ ASP/Pages/Strefa_Administracji/Logowanie.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Narciarze_v_2.Pages
+{
+    public class Logowanie_Administracja_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+    }
+}
