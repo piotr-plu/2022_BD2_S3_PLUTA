18401550822e4b49063671e6ed4dd4111137aff7
aang083 <golen46@gmail.com>
2023-01-02 12:37:45 +0100

    Work Update

--- ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Klienta/Raport_klient.cshtml.cs
+++ ASP_RAZORY/Narciarze_v_2/Pages/Strefa_Klienta/Raport_klient.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Narciarze_v_2.Pages.Strefa_Administracji.Administrator
+namespace Narciarze_v_2.Pages.Strefa_Klienta
 {
-    public class GrupowanieStokowModel : PageModel
+    public class Raport_klientModel : PageModel
     {
         public void OnGet()
         {
