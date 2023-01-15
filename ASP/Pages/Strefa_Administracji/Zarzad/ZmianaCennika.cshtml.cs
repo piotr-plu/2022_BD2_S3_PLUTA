af6577216cace6c8ce73aa9afcce13c9b8c64e28
Cezary <Cezary@DESKTOP-L54I9S2>
2023-01-15 01:00:58 +0100

    Edycja Cennika, część biletowa

--- ASP/Pages/Strefa_Administracji/Zarzad/ZmianaCennika.cshtml.cs
+++ ASP/Pages/Strefa_Administracji/Zarzad/ZmianaCennika.cshtml.cs
@@ -59,7 +59,7 @@ namespace Narciarze_v_2.Pages.Strefa_Administracji.Zarzad
             }
             Wyciag w2 = new Wyciag();
             w2.id = Request.Form["wyciag"].ToString();
-            string query2 = "SELECT c.ID as id, c.Data_rozp as rozp, c.Data_zak as zak FROM Cennik as c, Cena_bilety as cb, Wyciagi as w WHERE c.ID_Cena_bilet = cb.ID AND w.ID = cb.ID_Wyciag AND c.Data_rozp < '2023-01-14' AND(c.Data_zak > '2023-01-14' OR c.Data_zak IS NULL) AND w.ID = '"+w2.id+"'";
+            string query2 = "SELECT c.ID as id, c.Data_rozp as rozp, c.Data_zak as zak FROM Cennik as c, Cena_bilety as cb, Wyciagi as w WHERE c.ID_Cena_bilet = cb.ID AND w.ID = cb.ID_Wyciag AND ((c.Data_rozp < '2023-01-14' AND c.Data_zak > '2023-01-14') OR c.Data_zak IS NULL) AND w.ID = '"+w2.id+"'";
             using (SqlCommand command = new SqlCommand(query2, conn))
             {
                 using (SqlDataReader reader = command.ExecuteReader())
@@ -192,34 +192,100 @@ namespace Narciarze_v_2.Pages.Strefa_Administracji.Zarzad
         {
             SqlConnection conn = new SqlConnection("Data Source=DESKTOP-L54I9S2\\NARCIARZE;Initial Catalog=narty;Integrated Security=True");
             conn.Open();
-            Edycja ee1= new Edycja();
-            ee1.id = Request.Form["wyciag"].ToString();
-            //string query8 = "SELECT min(Data_zak) as mini, w.Nazwa as nazwa FROM Cennik as c, Wyciagi as w, Cena_bilety as cb WHERE c.ID_Cena_bilet = cb.ID AND w.ID = cb.ID_Wyciag AND w.ID = '" + ee1.id+ "' GROUP BY w.Nazwa";
-            //using (SqlCommand command = new SqlCommand(query8, conn))
-            //{
-            //    using (SqlDataReader reader = command.ExecuteReader())
-            //    {
-            //        while (reader.Read())
-            //        {
-            //            ee1.nazwa = reader["nazwa"].ToString();
-            //            ee1.gr = reader["mini"].ToString();
-            //            var date1 = DateTime.Parse(ee1.gr);
-            //            ee1.gr = date1.Year.ToString() + "-" + date1.Month.ToString() + "-" + date1.Day.ToString();
-            //            ee.Add(ee1);
-            //        }
-            //    }
-            //}
-
+            Edycja e3= new Edycja();
+            e3.id = Request.Form["wyciag"].ToString();
+            string query8 = "SELECT Nazwa as nazwa FROM Wyciagi WHERE ID = '"+e3.id+"'";
+            using (SqlCommand command = new SqlCommand(query8, conn))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Edycja ee1 = new Edycja();
+                        ee1.nazwa = reader["nazwa"].ToString();
+                        ee.Add(ee1);
+                    }
+                }
+            }
         }
 
         public void OnPostDodaj()
         {
             SqlConnection conn = new SqlConnection("Data Source=DESKTOP-L54I9S2\\NARCIARZE;Initial Catalog=narty;Integrated Security=True");
             conn.Open();
+            Licznik l1 = new Licznik();
             Edycja e4 = new Edycja();
-            e4.nazwa = Request.Form["nazw"].ToString();
+            Cennik c3 = new Cennik();
+            e4.nazwa = Request.Form["nazwa"].ToString();
             e4.rozp = Request.Form["rozp"].ToString();
             e4.cena = Request.Form["cena"].ToString();
+            string query9 = "SELECT c.ID as id, c.Data_rozp as rozp FROM Cennik as c, Wyciagi as w, Cena_bilety as cb WHERE c.ID_Cena_bilet = cb.ID AND w.ID = cb.ID_Wyciag AND c.Data_zak IS NULL AND w.Nazwa = '" + e4.nazwa + "'";
+            using (SqlCommand command = new SqlCommand(query9, conn))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        c3.id = reader["id"].ToString();
+                        c3.rozp = reader["rozp"].ToString();
+                    }
+                }
+            }
+            var date1 = DateTime.Parse(c3.rozp);
+            var date2 = DateTime.Parse(e4.rozp);
+            var diff = date2- date1;
+            int idiff = diff.Days;
+            if (idiff <= 0)
+            {
+                Console.WriteLine("B³¹d");
+                //Data nowego cennika jest przed dat¹ starego, mo¿na wyrzuciæ jakiœ b³¹d na ekran
+            }
+            else
+            {
+                e4.zak = date2.Year.ToString() + "-" + date2.Month.ToString() + "-" + date2.Day.ToString();
+                string query10 = "UPDATE Cennik SET Data_zak = '" + e4.zak + "' WHERE ID = '"+c3.id+"'";
+                using (SqlCommand command = new SqlCommand(query10, conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+                string querya = "SELECT COUNT(cb.ID) as liczba FROM Cena_bilety as cb, Wyciagi as w WHERE w.ID = cb.ID_Wyciag AND w.Nazwa = '" + e4.nazwa + "' AND cb.Cena_przejazd = '" + e4.cena + "'";
+            using (SqlCommand command = new SqlCommand(querya, conn))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            l1.liczba = reader["liczba"].ToString();
+                        }
+                    }
+                }
+                int l = int.Parse(l1.liczba);
+                if (l == 0)
+                {
+                    string query5 = "INSERT INTO Cena_bilety (ID_Wyciag, Cena_przejazd) VALUES ((SELECT ID FROM Wyciagi WHERE Nazwa = '" + e4.nazwa + "'), '" + e4.cena + "')";
+                    using (SqlCommand command = new SqlCommand(query5, conn))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                string query11 = "SELECT cb.ID as id FROM Cena_bilety as cb, Wyciagi as w WHERE w.ID = cb.ID_Wyciag AND cb.Cena_przejazd = '"+e4.cena+"' AND w.Nazwa = '"+e4.nazwa+"'";
+                using (SqlCommand command = new SqlCommand(query11, conn))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            e4.id = reader["id"].ToString();
+                        }
+                    }
+                }
+                string query12 = "INSERT INTO Cennik (ID_Cena_bilet, Data_rozp) VALUES ('" + e4.id + "', '" + e4.zak + "')";
+                using (SqlCommand command = new SqlCommand(query12, conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+                Response.Redirect("ZmianaCennika");
+            }
         }
 
         //public void OnPostAkt()
