1bc8658b5191bca3d28b52acc9160dffcade2e1d
Piotr Pluta <piotplu356@studia.polsl.pl>
2023-01-25 19:07:54 +0100

    Instrukcja Klient

---
+++ ASP/Pages/Strefa_Klienta/PomocKlient.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Narciarze_v_2.Pages.Strefa_Klienta
+{
+    public class PomocKlientModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
